a13662867ac0284e4f25c61e235770744091ba99
Deejay Geroso <deejay@adally.com>
2020-06-15 15:32:51 +0800

    declaration.d.ts

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -2,3 +2,4 @@
  * This removes the error appearing on VSCode when importing images.
 /* ---------------------------------------------------------------------------------- */
 declare module '*.jpg'
+declare module '*.png'
