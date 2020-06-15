b0bb232fbab866c751f5b6ec75023420b29db07e
Deejay Geroso <deejay@adally.com>
2020-06-15 18:11:15 +0800

    Updated default font color

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -3,12 +3,14 @@ import Projects from './Projects'
 import { RouteComponentProps } from '@reach/router'
 import WelcomeScreen from './WelcomeScreen'
 import './styles.css'
+import Testimonials from './Testimonials/Testimonials'
 
 const Home: FunctionComponent<RouteComponentProps> = (): ReactElement => {
   return (
     <>
       <WelcomeScreen />
       <Projects />
+      <Testimonials />
     </>
   )
 }
---
+++ src/modules/Home/Testimonials/Testimonials.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent, ReactElement } from 'react'
+
+interface IProps {}
+
+const Testimonials: FunctionComponent<IProps> = (props: IProps): ReactElement => {
+  return (
+    <section id='testimonials'>
+      {/* <img /> */}
+      <p>Testimonials</p>
+    </section>
+  )
+}
+
+export default Testimonials
