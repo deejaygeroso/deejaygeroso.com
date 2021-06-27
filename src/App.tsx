c1c51d5a038e91d7bec172cf036f61188cabc67a
Deejay Geroso <deejaygeroso@gmail.com>
2021-06-27 11:20:17 +0800

    Removed reach/router comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement } from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Blog from './modules/Blog'
 import Home from './modules/Home'
-// import { Router } from '@reach/router'
 import './common/styles/app.css'
 
 const App = (): ReactElement => {
