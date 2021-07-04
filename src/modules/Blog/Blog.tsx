9a1e4d469435737be590658fbd0974d9341c49cd
Deejay Geroso <deejaygeroso@gmail.com>
2021-07-04 15:38:34 +0800

    Removed navigation component

--- src/modules/Blog/Blog.tsx
+++ src/modules/Blog/Blog.tsx
@@ -1,12 +1,10 @@
 import React, { FunctionComponent, ReactElement } from 'react'
-import { Navigation } from '../../common/components'
 import { RouteComponentProps } from '@reach/router'
 import './styles.css'
 
 const Blog: FunctionComponent<RouteComponentProps> = (): ReactElement => {
   return (
     <>
-      <Navigation />
       <h1>My Blog</h1>
     </>
   )
