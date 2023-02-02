6b37b1d8947b065c5f6d79484cecefdf2966282e
Deejay Geroso <deejaygeroso@gmail.com>
2023-02-03 01:35:49 +0800

    New: Reconfigure linting rules and prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,20 @@ module.exports = {
   arrowParens: 'avoid',
   bracketSpacing: true,
   endOfLine: 'auto',
+  /*
+   * importOrder is an extension of @trivago/prettier-plugin-sort-imports package
+   */
+  importOrder: [
+    'react(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '^@/common/(.*)$',
+    '^@/context/(.*)$',
+    '^@/data/(.*)$',
+    '^@/pages/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
   jsxBracketSameLine: true,
   jsxSingleQuote: true,
   parser: 'typescript',
