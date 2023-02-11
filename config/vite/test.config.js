2eb3d83d439dd967707b62d9b14d34c610b29946
Deejay Geroso <deejaygeroso@gmail.com>
2023-02-12 00:13:14 +0800

    Chore: Resolved indention

--- config/vite/test.config.js
+++ config/vite/test.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, '../../src'),
-		},
-	},
-	test: {
-		environment: 'jsdom',
-		globals: true,
-		setupFiles: path.resolve(__dirname, '../../config/vite/setup.js'),
-	}
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '../../src'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: path.resolve(__dirname, '../../config/vite/setup.js'),
+  },
 })
